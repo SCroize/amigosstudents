8c439e466bc5f3a41ef816a2a08c5c9b2d5f2269
SCroize <Mahagoni19>
2021-06-15 18:11:50 +0200

    Add Simple RESTfull API (Hello World)

--- src/main/java/com/example/amigosstudents/AmigosstudentsApplication.java
+++ src/main/java/com/example/amigosstudents/AmigosstudentsApplication.java
@@ -2,12 +2,20 @@ package com.example.amigosstudents;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController //Makes this class to serve as REST Endpoints
 public class AmigosstudentsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AmigosstudentsApplication.class, args);
+    }
+
+    @GetMapping
+    public String hello() {
+        return "Hello World";
     }
 
 }
