ccc81aa19d1afa83826eb03e1890aeaecc7497bd
SCroize <Mahagoni19>
2021-06-15 18:48:51 +0200

    Add @Service and @Autowired for dependency injection

---
+++ src/main/java/com/example/amigosstudents/student/StudentService.java
@@ -0,0 +1,21 @@
+package com.example.amigosstudents.student;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Service //Or @Component , makes this class a bean that can be instantiated with @Autowired
+public class StudentService {
+    public List<Student> getStudents() {
+        return List.of(
+                new Student(
+                        1L,
+                        "Mariam",
+                        "mariam.jamal@gmail.com",
+                        LocalDate.of(2000, Month.JANUARY, 5),
+                        21));
+    }
+}
