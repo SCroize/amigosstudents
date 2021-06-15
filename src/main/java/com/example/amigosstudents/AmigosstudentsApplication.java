a474e4eb39e66a7fd645dae57102a0d4c6dfd692
SCroize <Mahagoni19>
2021-06-15 17:54:30 +0200

    Initial Commit

---
+++ src/main/java/com/example/amigosstudents/AmigosstudentsApplication.java
@@ -0,0 +1,13 @@
+package com.example.amigosstudents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmigosstudentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmigosstudentsApplication.class, args);
+    }
+
+}
