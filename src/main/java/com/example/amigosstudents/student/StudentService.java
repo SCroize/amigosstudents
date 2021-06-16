40f67bbccef2d535a755f6072a5e8931b9ba88c9
SCroize <Mahagoni19>
2021-06-16 19:43:15 +0200

    Modify StudentService to return Data from StudentRepository

--- src/main/java/com/example/amigosstudents/student/StudentService.java
+++ src/main/java/com/example/amigosstudents/student/StudentService.java
@@ -1,5 +1,6 @@
 package com.example.amigosstudents.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -8,13 +9,15 @@ import java.util.List;
 
 @Service //Or @Component , makes this class a bean that can be instantiated with @Autowired
 public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents() {
-        return List.of(
-                new Student(
-                        1L,
-                        "Mariam",
-                        "mariam.jamal@gmail.com",
-                        LocalDate.of(2000, Month.JANUARY, 5),
-                        21));
+        return studentRepository.findAll();
     }
 }
