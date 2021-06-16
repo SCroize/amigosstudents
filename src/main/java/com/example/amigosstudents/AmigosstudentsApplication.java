56b143f06be24db989b9a2d69b1e9efcd4402fa9
SCroize <Mahagoni19>
2021-06-16 19:01:08 +0200

    Add PostgreSQL Database to project

--- src/main/java/com/example/amigosstudents/AmigosstudentsApplication.java
+++ src/main/java/com/example/amigosstudents/AmigosstudentsApplication.java
@@ -1,14 +1,8 @@
 package com.example.amigosstudents;
 
-import com.example.amigosstudents.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.List;
 
 @SpringBootApplication
 @RestController //Makes this class to serve as REST Endpoints
@@ -17,7 +11,5 @@ public class AmigosstudentsApplication {
     public static void main(String[] args) {
         SpringApplication.run(AmigosstudentsApplication.class, args);
     }
-
-
 
 }
